e2c8a01872db8436399538acf25daea7bf8bfb0f
Dan Fornika <dfornika@gmail.com>
2015-10-29 12:41:15 -0700

    Added eval-replace `C-c e`

--- init.el
+++ init.el
@@ -61,3 +61,16 @@
   (if (= prefix 1) (switch-to-next-buffer)))
 (bind-key "C-x 2" 'my/vsplit-last-buffer)
 (bind-key "C-x 3" 'my/hsplit-last-buffer)
+
+;; Evaluate an emacs lisp expression and replace it by its result.
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+    (prin1 (eval (read (current-kill 0)))
+      (current-buffer))
+    (error (message "Invalid expression")
+      (insert (current-kill 0)))))
+
+(global-set-key (kbd "C-c e") 'eval-and-replace)
