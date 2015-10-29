8792bbc979ad41863b8725d42938c868173ced3d
Dan Fornika <dfornika@gmail.com>
2015-10-29 12:35:07 -0700

    Updated url to melpa repo

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t))
 
 ;; Remap navigation keys to the home row
 (global-unset-key (kbd "C-p"))
