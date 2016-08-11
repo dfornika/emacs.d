0ff02d3ac00aedecdc440b0d1d0893fb3f3a9eff
Dan Fornika <dfornika@gmail.com>
2016-08-11 10:43:31 -0700

    Update init.el
    Added some CIDER configuration, taken from:
    http://fgiasson.com/blog/index.php/2016/06/14/my-optimal-gnu-emacs-settings-for-developing-clojure-revised/

--- init.el
+++ init.el
@@ -94,6 +94,20 @@
 (bind-key "C-x 2" 'my/vsplit-last-buffer)
 (bind-key "C-x 3" 'my/hsplit-last-buffer)
 
+;; Enter cider mode when entering the clojure major mode
+(add-hook 'clojure-mode-hook 'cider-mode)
+
+;; Turn on auto-completion with Company-Mode
+(global-company-mode)
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+
+;; Replace return key with newline-and-indent when in cider mode.
+(add-hook 'cider-mode-hook '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
+
+;; Show parenthesis mode
+(show-paren-mode 1)
+
 ;; Evaluate an emacs lisp expression and replace it by its result.
 (defun eval-and-replace ()
   "Replace the preceding sexp with its value."
