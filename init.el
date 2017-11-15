b2a5d630a72e6484c612b72d0e24906484b6b0d4
Dan Fornika <dfornika@gmail.com>
2017-11-15 07:17:01 -0800

    Always install multiple-cursors

--- init.el
+++ init.el
@@ -1,10 +1,19 @@
 ;; Make emacs less annoying
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq inhibit-splash-screen t)
 (setq ring-bell-function 'ignore)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 ;;
 (setq sentence-end-double-space nil)
+
+(setq user-init-dir "~/.emacs.d/")
 
 (defun load-user-file (file)
   (interactive "f")
@@ -51,3 +60,15 @@
       (insert (current-kill 0)))))
 
 (global-set-key (kbd "C-c e") 'eval-and-replace)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (multiple-cursors use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- packages.el
+++ packages.el
@@ -16,3 +16,4 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package multiple-cursors)
