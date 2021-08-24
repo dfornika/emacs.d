68691788a2cd612f02e878b4e23e12aff91dbc1e
Dan Fornika <dfornika@gmail.com>
2021-08-24 08:08:53 -0700

    Disable toolbar, add transient dir to gitignore

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (setq inhibit-splash-screen t)
+(tool-bar-mode -1)
 (setq ring-bell-function 'ignore)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
@@ -6,8 +7,16 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; Bootstrap 'use-package'
+(eval-after-load 'gnutls
+  '(add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem"))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 (eval-when-compile
   (require 'use-package))
+(require 'bind-key)
+(setq use-package-always-ensure t)
 
 (use-package doom-themes
   :ensure t
@@ -32,9 +41,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" default)))
- '(package-selected-packages (quote (doom-themes cider cargo rust-mode dracula-theme))))
+   '("947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" default))
+ '(package-selected-packages '(doom-themes cider cargo rust-mode dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
